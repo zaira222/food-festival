31e28debc898f1a25289d414d66737be11d9ed53
ZP <zairaperez01@gmail.com>
2022-04-03 21:51:50 -0500

    optimized images with webpack

---
+++ assets/js/helpers.js
@@ -0,0 +1,15 @@
+dateConverter = function(UNIX_timestamp) {
+    const a = new Date(UNIX_timestamp);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const result =  month + ' ' + date + ', ' + year 
+    return result;
+  };
+
+
+module.exports = {
+    dateConverter,
+    createLoremIpsum
+  }
